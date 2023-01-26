35f287edf354f19be9f1de7ac4b27e50f83fd79f
svianac <31249803+svianac@users.noreply.github.com>
2023-01-26 18:22:24 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,5 @@ echo "Starting to build the component:"
 echo "Create target folder:"
 mkdir ../target
 echo "Create artefact:"
-cp ../sourcecode/CBC_SplitterFile.java ../target/CBC_SplitterFile.jar
+cp sourcecode/CBC_SplitterFile.java target/CBC_SplitterFile.jar
 echo "Done."
